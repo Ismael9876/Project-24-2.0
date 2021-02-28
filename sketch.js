7b474402b847c7786832c1329290779b8c7e9cf5
Ismael9876 <77596446+Ismael9876@users.noreply.github.com>
2021-02-28 16:35:05 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var wall1;
+var wall2;
+var wall3;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+  
+  
+  wall1 = createSprite(780, 600, 20, 110);
+  wall2 = createSprite(680, 645, 190, 20);
+  wall3 = createSprite(590, 600, 20, 110);
+  
+	engine = Engine.create();
+	world = engine.world;
+
+  
+
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  background(0);
+
+  ellipse(130, 645, 55, 55);
+  
+  wall1.shapeColor = ("white");
+  wall2.shapeColor = ("white");
+  wall3.shapeColor = ("white");
+  
+
+  drawSprites();
+ 
+}
+
+function Pressed(){
+if (keyCode === UP_ARROW){
+  Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+}
+
+}
